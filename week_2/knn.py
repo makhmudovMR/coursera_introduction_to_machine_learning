4c644c962087c127d10f1647c80f65d07e1825d8
Magomed Makhmudov <mmriadovich@gmail.com>
2018-06-30 17:29:48 +0300

    knn

--- week_2/knn.py
+++ week_2/knn.py
@@ -1,6 +1,8 @@
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import Counter
+
 
 def euclidian_distances(x,y):
     x, y = np.array(x), np.array(y)
@@ -15,16 +17,19 @@ def knn(data, predict, k=1):
     distances = []
     for group in data:
         for feature in data[group]:
-            dist = euclidian_distances(feature, predict)
-            distances.append([dist, group])
+            # dist = euclidian_distances(feature, predict)
+            euclid_dist = np.linalg.norm(np.array(feature) - np.array(predict))
+            distances.append([euclid_dist, group])
+    votes = [i[1] for i in sorted(distances)[:k]]
+    print(Counter(votes).most_common(1))
+    vote_result = Counter(votes).most_common(1)[0][0]
 
-    neighbors = distances[:k]
-    print(neighbors)
-    pass
+    return vote_result
+
 
 def main():
     dataset = {'k': [[1, 2], [2, 1], [3, 2], [3, 2]], 'r': [[6, 7], [9, 5], [6, 5], [9, 9]]}
-    knn(dataset, [1,3])
+    print(knn(dataset, [1,3], k=3))
 
 
 if __name__ == '__main__':
